ea818856bc419d35a6f8d2d39d790aebb0f5c7a4
Kenny Pham <kennypham@kennys-mbp-2.attlocal.net>
2021-06-27 01:33:28 -0700

    refactored our StreamEdit component

--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -16,7 +16,7 @@ const streamReducer = (state = {}, action) => {
 		case CREATE_STREAM:
 			return { ...state, [action.payload.id]: action.payload };
 		case EDIT_STREAM:
-			return { ...state, [action.paylod.id]: action.payload };
+			return { ...state, [action.payload.id]: action.payload };
 		case DELETE_STREAM:
 			return _.omit(state, action.payload);
 		default:
